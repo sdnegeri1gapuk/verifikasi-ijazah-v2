28f2b2322d03917b32425feea6ec56a96b42ff58
sdn1gapuk <sdn01gapuk@gmail.com>
2026-07-18 12:01:35 +0800

    Perbaiki multipage Vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    base: "/verifikasi-ijazah-v2/"
+  base: "/verifikasi-ijazah-v2/",
+
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "login.html"),
+        dashboard: resolve(__dirname, "dashboard.html"),
+        verifikasi: resolve(__dirname, "verifikasi.html"),
+      },
+    },
+  },
 });
