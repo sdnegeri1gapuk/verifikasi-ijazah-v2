c2e2769d01316316ebff73c2378f8267d9b9b6cc
sdnegeri1gapuk <sdnegeri1gapuk@gmail.com>
2026-07-20 11:41:38 +0800

    Adjust QR image Y position in upload.js

--- src/js/modules/upload.js
+++ src/js/modules/upload.js
@@ -24,7 +24,7 @@ export async function tempelQRKePDF(file, qrBlob, jenisDokumen) {
     page.drawImage(qrImage, {
 
         x: width - 250,
-        y: 147,
+        y: 105,
         width:80,
         height:80
 
